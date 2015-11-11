7732677ebaac37d13b05e1ad342420b2c4b080e6
Franku <sz.frankyy@gmail.com>
2015-11-11 20:26:31 +0100

    formatting

--- libs/getOneVideo.js
+++ libs/getOneVideo.js
@@ -1,53 +1,23 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').exec;
+var exec = require('child_process').exec,
+    argv = require('optimist').argv,
+    child,
+    dir = argv.d,
+    url = argv.u,
+    F = argv.F,
+    paramsYTLString = argv.paramsYTLString;
 
-var argv = require('optimist').argv;
-var child;
-
-var dir=argv.d;
-var url=argv.u;
-var F=argv.F;
-var paramsYTLString=argv.paramsYTLString;
-
-//console.log(argv);
-
-//console.log('cd '+dir+' && youtube-dl '+paramsYTLString+' '+url +' | less');
 
 if (F) {
-   // console.log('youtube-dl -F '+url + '| less');
-    child=exec('youtube-dl -F '+url + '| less',function(data){
-        
-        
-    });
-
-  // child.stdout.pipe(process.stdout);    
-    
-    
-
+    child = exec('youtube-dl -F ' + url + '| less', function (data) {});
 } else {
-    
-  console.log('cd '+dir+' && youtube-dl '+url +' | less');  
-  child=exec('cd '+dir+' && youtube-dl '+url +' | less');   
+    console.log('cd ' + dir + ' && youtube-dl ' + url + ' | less');
+    child = exec('cd ' + dir + ' && youtube-dl ' + url + ' | less');
 }
-
-
-
-//child=exec('cd '+dir+' && youtube-dl '+paramsYTLString+' '+url +' | less',function(err,stdout,stderr){
-
-							
-//});
-
-
-
-
 
 child.stdout.pipe(process.stdout);
 
-
-child.on('exit',function(data){
-    
+child.on('exit', function (data) {
     console.log('Downloaded!');
-    });
-
-
+});
--- run.js
+++ run.js
@@ -1,65 +1,44 @@
-var http = require('https');
-var cheerio = require('cheerio');
-var sys = require('sys');
-var exec = require('child_process').exec;
-var argv = require('optimist').argv;
-var getList=require('./libs/getList').getList;
-
-
+var http = require('https'),
+    cheerio = require('cheerio'),
+    sys = require('sys'),
+    exec = require('child_process').exec,
+    argv = require('optimist').argv,
+    getList = require('./libs/getList').getList;
 
 //node run.js -F --ytl-u PLSPScJcnp4W6lC0vME0UuCDTQ4W2h5cg9
 
 
 
 
-
-
-
-
 /* params passed to youtube-dl */
-var paramsYTL={};
+var paramsYTL = {};
 /* params for yt_list */
-var paramsYTDL={};
+var paramsYTDL = {};
 
 /* divide command line parameters for paramsYTL and paramsYTDL */
 for (var prop in argv) {
-    if (argv.hasOwnProperty(prop) && prop!=='_' && prop!=='$0') {
-	
-
-	if(prop.substr(0,3)==='ytl'){
-	    paramsYTDL[prop.substr(4)]=argv[prop];    
-	    
-	} else {
-	    paramsYTL[prop]=argv[prop];
-	    
-	}
-	
+    if (argv.hasOwnProperty(prop) && prop !== '_' && prop !== '$0') {
+        if (prop.substr(0, 3) === 'ytl') {
+            paramsYTDL[prop.substr(4)] = argv[prop];
+        } else {
+            paramsYTL[prop] = argv[prop];
+        }
     }
 }
 
-
-if (paramsYTDL.d===undefined) {
-    paramsYTDL.d='.';
+if (paramsYTDL.d === undefined) {
+    paramsYTDL.d = '.';
 }
 
 /*if the full adress has not been given let us complete it*/
-if (((paramsYTDL.u).slice(0,24)!=='https://www.youtube.com/')) {
-    paramsYTDL.u='https://www.youtube.com/playlist?list='+paramsYTDL.u;
+if (((paramsYTDL.u).slice(0, 24) !== 'https://www.youtube.com/')) {
+    paramsYTDL.u = 'https://www.youtube.com/playlist?list=' + paramsYTDL.u;
 }
+
 console.log(paramsYTDL.u);
 
 if (paramsYTDL.c) {
-    getList(paramsYTDL,{F:''});
+    getList(paramsYTDL, {F: ''});
 }
 
-
-
-getList(paramsYTDL,paramsYTL);
-
-
-
-
-
-
-
-
+getList(paramsYTDL, paramsYTL);
